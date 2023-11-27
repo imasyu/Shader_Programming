f572fdd5d7ffff69d6d565642dd54875a40cea7f
偉央 小野寺 <220342@jc-21.jp>
2023-11-27 10:10:27 +0900

    ライディングの途中まで変更

--- Engine/Quad.cpp
+++ Engine/Quad.cpp
@@ -184,6 +184,7 @@ void Quad::PassDataToCB(Transform transform)
 	CONSTANT_BUFFER cb;
 	cb.matWVP = XMMatrixTranspose(transform.GetWorldMatrix() * Camera::GetViewMatrix() * Camera::GetProjectionMatrix());
 	cb.matNormal = XMMatrixTranspose(transform.GetNormalMatrix());
+	cb.wSize = pTexture_->GetTextureSize();
 
 	D3D11_MAPPED_SUBRESOURCE pdata;
 	Direct3D::pContext_->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	// GPUからのデータアクセスを止める
